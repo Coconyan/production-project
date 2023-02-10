d08ea491de9190aed7e29d2ebe613d4942966ae7
Coconyan <San3k19@gmail.com>
2023-02-10 21:46:58 +0300

    work with styles, add site Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme to {theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
+      <br />
       <Link to={'/'}>Main Page</Link>
       <Link to={'/about'}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
