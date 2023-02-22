31a4cceadc85f0684b25a583dac4e9ce7c1549d4
Coconyan <San3k19@gmail.com>
2023-02-22 22:48:09 +0300

    add HotModuleReplacementPlugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,12 +6,9 @@ import { IBuildOptions } from "./types/config";
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
   return {
-    // static: {
-    //   directory: path.join(__dirname, 'public'),
-    // },
-
     port: options.port,
     open: true,
     historyApiFallback: true,
+    hot: true,
   }
 }
