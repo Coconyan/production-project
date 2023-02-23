d348bb54a57f17764f0a11f3d5c11e273c22d472
Coconyan <San3k19@gmail.com>
2023-02-23 14:14:27 +0300

    remove spaces

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,8 +1,5 @@
-import path from "path";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import { IBuildOptions } from "./types/config";
-
-
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
   return {
